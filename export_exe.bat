5f86a66c37cd5da4c824c8b92ae5d5812ac27ae0
jmhuo <jmhuo@corp.netease.com>
2021-04-02 15:59:41 +0800

    转换id

--- export_exe.bat
+++ export_exe.bat
@@ -2,11 +2,11 @@
 cd %~dp0
 set ROOT_DIR=%~dp0
 
-pyinstaller -F src/convert_idcard.py
+pyinstaller -F  src/convert_idcard.py
 
 
 copy %BIN_DIR%src\config.json %BIN_DIR%dist\config.json
-copy %BIN_DIR%src\订单白名单.xlsx %BIN_DIR%dist\订单白名单.xlsx
-copy %BIN_DIR%src\使用说明.txt %BIN_DIR%dist\使用说明.txt
+copy %BIN_DIR%src\order_white_list.xlsx %BIN_DIR%dist\order_white_list.xlsx
+copy %BIN_DIR%src\readme.txt %BIN_DIR%dist\readme.txt
 
 
