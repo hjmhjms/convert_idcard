3c9d3b391a18c3033c3d4914783e449054d94daa
jmhuo <jmhuo@corp.netease.com>
2021-04-02 15:52:33 +0800

    转换id

--- export_exe.bat
+++ export_exe.bat
@@ -1,5 +1,12 @@
+::%~dp0
+cd %~dp0
+set ROOT_DIR=%~dp0
+
+pyinstaller -F src/convert_idcard.py
 
 
+copy %BIN_DIR%src\config.json %BIN_DIR%dist\config.json
+copy %BIN_DIR%src\订单白名单.xlsx %BIN_DIR%dist\订单白名单.xlsx
+copy %BIN_DIR%src\使用说明.txt %BIN_DIR%dist\使用说明.txt
 
-%~dp0
-pyinstaller -F convert_idcard.py --specpath
+
--- src/convert_idcard.py
+++ src/convert_idcard.py
@@ -2,6 +2,8 @@ import PIL.Image as Image
 import os
 import shutil
 import time
+import xlrd
+import math
 
 
 # 1268370590336878595-孙剑文-310107198103161325-1.jpg
@@ -27,7 +29,7 @@ def _CalcAllIdCardFiles(szSrcDir):
     return listIdCardFiles
 
 
-def ConvertIDCards(szSrcDir, szTargetDir):
+def ConvertIDCards(szSrcDir, szTargetDir, dictWhiteList):
     fromAbsPath = os.path.abspath(szSrcDir)
     toAbsPath = os.path.abspath(szTargetDir)
     if not os.path.isdir(fromAbsPath):
@@ -44,6 +46,10 @@ def ConvertIDCards(szSrcDir, szTargetDir):
     for szItem in listAll:
         try:
             orderid, name, idcard = szItem.split('-')
+
+            if dictWhiteList and orderid not in dictWhiteList:
+                continue
+
             srcImage1 = os.path.join(fromAbsPath, f"{szItem}-1.jpg")
             srcImage2 = os.path.join(fromAbsPath, f"{szItem}-2.jpg")
             targetImage = os.path.join(toAbsPath, f"{idcard}.jpg")
@@ -74,20 +80,116 @@ def _ConvertOneIdCard(szImage1Path, szImage2Path, szTargetPath):
 def GetConfig():
     curDir = os.path.dirname(os.path.abspath(__file__))
     configFile = os.path.join(curDir, "config.json")
+    xlsFile = os.path.join(curDir, "订单白名单.xlsx")
+
     import json
     try:
-        with open(configFile, "r") as FileObj:
+        with open(configFile, "r", encoding="utf-8") as FileObj:
             configJson = json.load(FileObj)
-            return configJson["input_dir"], configJson["output_dir"]
+            return os.path.abspath(configJson["需要合并的身份证图片放在哪里"]), os.path.abspath(configJson["合并的身份证图片放在哪里"]), os.path.abspath(configJson['订单白名单'])
     except Exception as ErrObj:
         print("load config", str(ErrObj))
 
-    return "E:/idcard", "E:/idcard/out"
+    return "E:/idcard", "E:/idcard/out", xlsFile
+
+
+def ReadExcelFileData(szReadPath):
+    # 打开excel文件
+    WorkBookObj = xlrd.open_workbook(szReadPath)
+    szSheetNames = WorkBookObj.sheet_names()
+    WorkSheetObj = WorkBookObj.sheet_by_name(szSheetNames[0])
+    print(szReadPath)
+
+    def _CheckIsInt(xCheckValue):
+        if isinstance(xCheckValue, float):
+            fLessZero, fBigZero = math.modf(xCheckValue)
+            if fLessZero == 0:
+                xCheckValue = int(xCheckValue)
+        return xCheckValue
+
+    def _GetSaveKey(szKey):
+        if isinstance(szKey, str) and szKey.endswith("]"):
+            nLefeSignIndex = szKey.find("[")
+            if nLefeSignIndex != -1:
+                return szKey[:nLefeSignIndex]
+        return szKey
+
+    def _IsEmptyValue(xValue):
+        if (xValue is None) or (isinstance(xValue, str) and len(xValue) == 0) or xValue == "":
+            return True
+        return False
+
+    dictFileData = {}
+
+    # 读取文件第一行
+    listColumnNames = []
+    for nColIndex in range(WorkSheetObj.ncols):
+        xCellValue = WorkSheetObj.cell_value(0, nColIndex)
+        if _IsEmptyValue(xCellValue):
+            listColumnNames.append(None)
+            continue
+        listColumnNames.append(xCellValue)
+
+    # 标记为key的顺序
+    listKeyIndex = [_GetSaveKey(szKey) for szKey in listColumnNames if szKey]
+
+    # 从第2行开始读取
+    for nRowsIndex in range(1, WorkSheetObj.nrows):
+        xRowKeyID = WorkSheetObj.cell_value(nRowsIndex, 0)
+        xRowKeyID = _CheckIsInt(xRowKeyID)
+        if _IsEmptyValue(xRowKeyID):
+            continue
+
+        if xRowKeyID in dictFileData:
+            print("重复ID: {}, 表{}".format(xRowKeyID, szReadPath))
+            continue
+
+        dictRowData = {}
+        for nColIndex in range(WorkSheetObj.ncols):
+            xCellValue = WorkSheetObj.cell_value(nRowsIndex, nColIndex)
+            xCellValue = _CheckIsInt(xCellValue)
+            if _IsEmptyValue(xCellValue):
+                continue
+
+            xCellKey = listColumnNames[nColIndex]
+            if xCellKey is None:
+                continue
+
+            dictRowData[xCellKey] = xCellValue
+
+        dictProcessData = {}
+        for xCellKey, xCellValue in dictRowData.items():
+            # 转换tuple_num, list_num等通用后处理
+            if isinstance(xCellKey, str) and xCellKey.endswith("]"):
+                for szFormatType in g_dictFormatKeyScript:
+                    if xCellKey.endswith(szFormatType):
+                        xCellValue = g_dictFormatKeyScript[szFormatType](xCellValue)
+                        xCellKey = xCellKey.replace(szFormatType, "")
+                        break
+
+                for szFormatType in g_dictFormatMultiKeyScript:
+                    if xCellKey.endswith(szFormatType):
+                        xCellValue = g_dictFormatMultiKeyScript[szFormatType](xCellValue, xCellKey, dictRowData)
+                        xCellKey = xCellKey.replace(szFormatType, "")
+                        break
+
+            dictProcessData[xCellKey] = xCellValue
+
+        dictFileData[xRowKeyID] = dictProcessData
+
+    return dictFileData, listKeyIndex
 
 
 if __name__ == '__main__':
-    szindir, szoutDir = GetConfig()
-    ConvertIDCards(szindir, szoutDir)
+    szindir, szoutDir, xlsFile = GetConfig()
+    print(szindir, szoutDir, xlsFile)
+    dictOrderWhiteList = {}
+    try:
+        dictOrderWhiteList, listKeyIndex = ReadExcelFileData(xlsFile)
+    except Exception as e:
+        print(e)
+
+    ConvertIDCards(szindir, szoutDir, dictOrderWhiteList)
     a = input('按回车键退出...')
 
     # ConvertIdCard("E:/idcard/1268370590336878595-孙剑文-310107198103161325.jpg-1.jpg", "E:/idcard/1268370590336878595-孙剑文-310107198103161325.jpg-2.jpg", "E:/idcard/Test.jpg")
