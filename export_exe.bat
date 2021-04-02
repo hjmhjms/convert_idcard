fb946efb1d0d0bf83eec3d9713ada7de70836039
jmhuo <jmhuo@corp.netease.com>
2021-04-02 16:01:59 +0800

    转换id

--- export_exe.bat
+++ export_exe.bat
@@ -7,6 +7,6 @@ pyinstaller -F  src/convert_idcard.py
 
 copy %BIN_DIR%src\config.json %BIN_DIR%dist\config.json
 copy %BIN_DIR%src\order_white_list.xlsx %BIN_DIR%dist\order_white_list.xlsx
-copy %BIN_DIR%src\readme.txt %BIN_DIR%dist\readme.txt
+copy %BIN_DIR%readme.txt %BIN_DIR%dist\readme.txt
 
 
